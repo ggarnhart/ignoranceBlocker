286f74df5f0e6aa0277e00ffb5a68ce2de2b5709
Greg Garnhart <gegarnhart@email.wm.edu>
2019-03-25 00:17:23 -0400

    initial prototype done.

---
+++ content.js
@@ -0,0 +1,21 @@
+$("body").prepend(`<div class="injection">
+<div class="col col-md">
+  <div class="jumbotron text-center">
+    <img src="https://media.canadianunderwriter.ca/uploads/2017/08/cuw-alert-icon.png" class="warning" />
+    <h1>Warning</h1>
+    <p>
+      This site has been marked as
+      <span class="bold">ignorant.</span> Visitors are frequently subject to
+      false claims, misinformation, and lies.
+    </p>
+    <br>
+    <button class="greg-button good-button">Take me to a safe site</button>
+    <button class="greg-button bad-button">
+      I accept the risk of ignorance
+    </button>
+    <br>
+    <br>
+  </div>
+</div>
+</div>
+`);
